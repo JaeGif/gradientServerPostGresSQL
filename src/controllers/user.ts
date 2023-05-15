9b0aa55922573827c2be77d4d39af0c7520dbb6d
Jae <giffordjacob0@gmail.com>
2023-05-16 07:56:11 +0800

    get user route added

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -6,7 +6,29 @@ export const users_get = async (
   res: Response,
   next: NextFunction
 ) => {
-  const users = await prisma.user.findMany({});
-  console.log(users);
-  res.json({ users }).status(200);
+  try {
+    const users = await prisma.user.findMany({});
+    res.json({ users }).status(200);
+  } catch (error) {
+    console.error(error);
+    res.sendStatus(404);
+  }
+};
+
+export const user_get = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const user = await prisma.user.findUnique({
+      where: {
+        id: req.params.id,
+      },
+    });
+    res.json({ user }).status(200);
+  } catch (error) {
+    console.error(error);
+    res.sendStatus(404);
+  }
 };
--- src/routes/user.ts
+++ src/routes/user.ts
@@ -3,5 +3,6 @@ const router = express.Router();
 import * as userController from '../controllers/user';
 
 router.get('/users', userController.users_get);
+router.get('/users/:id', userController.user_get);
 
 export default router;
