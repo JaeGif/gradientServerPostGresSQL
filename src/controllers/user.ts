cc78a7eccba28477e8f8b2b121f5135ea325d5aa
Jae <giffordjacob0@gmail.com>
2023-05-16 09:49:41 +0800

    put and delete methods added

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -32,3 +32,41 @@ export const user_get = async (
     res.sendStatus(404);
   }
 };
+
+export const user_put = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const data = JSON.parse(req.body);
+  try {
+    const user = await prisma.user.update({
+      where: {
+        id: req.params.id,
+      },
+      data,
+    });
+    res.json({ user }).status(200);
+  } catch (error) {
+    console.error(error);
+    res.sendStatus(404);
+  }
+};
+
+export const user_delete = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const user = await prisma.user.delete({
+      where: {
+        id: req.params.id,
+      },
+    });
+    res.json({ user }).status(200);
+  } catch (error) {
+    console.error(error);
+    res.sendStatus(404);
+  }
+};
